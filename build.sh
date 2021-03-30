c65644afa267245b52eac1bd8ec4d8cf869e2e78
robert-marik <marik@mendelu.cz>
2021-03-30 06:59:02 +0200

    Drobnosti

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ jupyter-book build .
 
 for i in `ls _build/html/*.html | grep -v index.html | grep -v search.html`
 do
-    echo "Substituce v souboru ",$i
+    # echo "Substituce v souboru ",$i
     csplit $i '/<\/head>/'
     cat xx00 replacement xx01 > $i
 done    
